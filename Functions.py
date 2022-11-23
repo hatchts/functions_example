d255681c74fdc27f7c290bde7b5ba4e2c9f6ce5c
hatchts <shatch86@gmail.com>
2022-11-22 19:02:50 -0700

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -70,3 +70,42 @@ print('That is not a valid unit')
 
 print_results(conv_number, conv_unit)
 
+# Conversion Calculator
+# By: Scott Hadzik
+
+# user input regarding the length to convert
+# get the unit from the user
+# convert the length to the correct unit
+# output the answer to the user
+def user_parser(user_input):
+# Do something
+# Separate the number from unit
+ values = user_input.rsplit(" ")
+print (values)
+
+while True:
+ user_input = input("number and unit to convert ")
+user_parser(user_input)
+# while True:
+# user_number = input("What number to convert? ")
+# if user_number.isdigit():
+# user_number = float(user_number)
+# break
+# else:
+# print ('please use a number')
+# user_unit = input("What unit is your number?")
+
+# if(user_unit == 'in'):
+# #perform in to mm
+# conv_number = user_number * 25.4
+# conv_unit = 'mm'
+# break
+# elif(user_unit == 'mm'):
+# #perform mm to in
+# conv_number = user_number / 25.4
+# conv_unit = 'in'
+# break
+# else:
+# print('That is not a valid unit')
+
+# print(conv_number, conv_unit)
