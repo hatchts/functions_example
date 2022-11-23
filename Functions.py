d80fa2a6f75319d76398d7418c707b53b505d463
hatchts <shatch86@gmail.com>
2022-11-22 17:46:10 -0700

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -1,52 +1,42 @@
 # Conversion Calculator
 # By: Stacy Hatch
 
-# User Input regarding length to convert
-# Get the Unit from the User
-# Convert the Length to the Correcd Unit
-# Output Answer to the User
+# Conversion Calculator
+# By: Scott Hadzik
 
-def user_parser(user_Input):
-    # Do Something
-    # Seperate the number from unit
-    user_input.rsplit("")
-
-
-    print('I will parse data')
+# user input regarding the length to convert
+# get the unit from the user
+# convert the length to the correct unit
+# output the answer to the user
+def user_parser(user_input):
+# Do something
+# Separate the number from unit
+ values = user_input.rsplit(" ")
+ print (values) 
 
 while True:
-    user_number = input("number and unit to convert ")
-    user_parser(user_number)
+ user_input = input("number and unit to convert ")
+user_parser(user_input)
+# while True:
+# user_number = input("What number to convert? ")
+# if user_number.isdigit():
+# user_number = float(user_number)
+# break
+# else:
+# print ('please use a number')
+# user_unit = input("What unit is your number?")
 
-   # while True:
-    # user_number = input("What number to convert?")
-   #  if user_number .isdigit():
-   #      user_number = float(user_number)
-    #     break
-   # else:
-    # print ('please use a number')
+# if(user_unit == 'in'):
+# #perform in to mm
+# conv_number = user_number * 25.4
+# conv_unit = 'mm'
+# break
+# elif(user_unit == 'mm'):
+# #perform mm to in
+# conv_number = user_number / 25.4
+# conv_unit = 'in'
+# break
+# else:
+# print('That is not a valid unit')
 
-    # user_unit = input("What unit is your number?")
-    
-
-    # to convert in to mm --> in x 25.4
-    # to convert mm to in --> mm / 25.4
-
-    # user gives in unit
-
-   # if(user_unit == 'in'):
-        # perform in to mm
-       # conv_number = user_number * 25.4
-      #  break
-
-  # elif(user_unit == 'mm'):
-        # perform mm to in
-       # conv_number = user_number / 25.4
-       # conv_unit = 'in'
-       # break
-    #else:
-    #    print('That is not a valid unit')
-
-
-    print(conv_number, conv_unit)
-    print(user_unit)
+# print(conv_number, conv_unit)
